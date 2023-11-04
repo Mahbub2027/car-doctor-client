2486d31754f18f3784c561b3c99cfea6c99115ab
Mahbub2027 <alam201127@gmail.com>
2023-11-04 12:57:12 +0600

    fetch data from mongodb database

--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -1,7 +1,8 @@
 import {AiOutlineArrowRight} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const ServicesCard = ({ service }) => {
-    const { img, title, price } = service;
+    const {_id, img, title, price } = service;
     return (
         <div className="card  bg-base-100 shadow-xl">
             <figure><img className="w-11/12 h-56" src={img} alt="Shoes" /></figure>
@@ -11,8 +12,10 @@ const ServicesCard = ({ service }) => {
                 <div className="card-actions justify-between items-center text-[#FF3811] text-lg ">
                     <p className=" font-semibold">Price: ${price}</p>
                     <p className=''><AiOutlineArrowRight></AiOutlineArrowRight></p>
+                    
                     {/* <button className="btn btn-primary">Buy Now</button> */}
                 </div>
+                <Link to={`/checkout/${_id}`}><button className='btn btn-warning'>Book now</button></Link>
             </div>
         </div>
     );
